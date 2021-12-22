9b193b3446a7a96619efde906bd25e971b6475a3
FAenX <kipronofb@gmail.com>
2021-12-22 10:02:46 +0300

    update bottom navigation

--- App.tsx
+++ App.tsx
@@ -7,93 +7,10 @@ import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from 'react-redux'
 import { HomeStack, SettingsStack } from './App/navigation/bottomNavigation/bottomNavigation';
 import {store}  from './App/redux/store'
-import { createMaterialBottomTabNavigator  } from '@react-navigation/material-bottom-tabs'
-import { MaterialIcons  } from '@expo/vector-icons';
-import { FontAwesome5 } from '@expo/vector-icons';
-import { MaterialCommunityIcons } from '@expo/vector-icons';
-import { Octicons } from '@expo/vector-icons';
-import { Fontisto } from '@expo/vector-icons';
-import { Ionicons } from '@expo/vector-icons';
-import { useNavigation } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Navigator from './App/navigation/bottomNavigation/bottomNavigation';
 
-// import ReactDOM from 'react-dom'
-const Tab = createBottomTabNavigator();
 
-const Navigator =()=> {
-  const navigation = useNavigation();  
-  return (
-    <>
-      <Tab.Navigator
-        initialRouteName="Home"
-        
-        
-        screenOptions={{
-          tabBarActiveTintColor: '#028373',
-          tabBarStyle: {
-            position: 'relative',
-            backgroundColor: '#fff',
-            height: 100
 
-          },
-          headerLeft: () => <Ionicons name="chevron-back" size={20} color="black" onPress={()=>navigation.goBack()} />,
-
-        }}
-        >
-        <Tab.Screen
-          name="Wallet"
-          component={HomeStack}
-          options={{
-            tabBarLabel: '',
-            tabBarIcon: ({ color, size }) => (
-              <FontAwesome5 name="wallet" size={30} color="#028373" />
-            ),
-          }}
-        />
-        <Tab.Screen
-          name="Contacts"
-          component={SettingsStack}
-          options={{
-            tabBarLabel: '',
-            tabBarIcon: ({ color, size }) => (
-              <MaterialCommunityIcons name="contacts" size={30} color="#028373" />
-            ),
-          }}
-        />
-        <Tab.Screen
-          name="Connect"
-          component={SettingsStack}
-          options={{
-            tabBarLabel: '',
-            tabBarIcon: ({ color, size }) => (
-              <MaterialIcons name="swap-vert-circle" size={60} color="#028373" />
-            ),
-          }}
-        />
-        <Tab.Screen
-          name="Notification"
-          component={SettingsStack}
-          options={{
-            tabBarLabel: '',
-            tabBarIcon: ({ color, size }) => (
-              <Octicons name="bell" size={30} color="#028373" />
-            ),
-          }}
-        />
-        <Tab.Screen
-          name="Settings"
-          component={SettingsStack}
-          options={{
-            tabBarLabel: '',
-            tabBarIcon: ({ color, size }) => (
-              <Fontisto name="player-settings" size={30} color="#028373" />
-            ),
-          }}
-        />
-      </Tab.Navigator>
-    </>
-  )
-}
 
 export default function App() {
   return (
--- App/navigation/bottomNavigation/bottomNavigation.tsx
+++ App/navigation/bottomNavigation/bottomNavigation.tsx
@@ -1,65 +1,91 @@
-// React Native Bottom Navigation
-// https://aboutreact.com/react-native-bottom-navigation/
-import * as React from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
-
-
-import SettingsScreen from '../../screens/screen1'
+// import ReactDOM from 'react-dom'
+import React from 'react';
+import { createMaterialBottomTabNavigator  } from '@react-navigation/material-bottom-tabs'
+import { MaterialIcons  } from '@expo/vector-icons';
+import { FontAwesome5 } from '@expo/vector-icons';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { Octicons } from '@expo/vector-icons';
+import { Fontisto } from '@expo/vector-icons';
+import { Ionicons } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from '../../screens/home/component';
 
+const Tab = createBottomTabNavigator();
 
-const Stack = createStackNavigator();
-
-
-export function HomeStack() {
+const Navigator =()=> {
+  const navigation = useNavigation();  
   return (
-    <Stack.Navigator
-      initialRouteName="Home"
-      screenOptions={{
-        headerStyle: { backgroundColor: '#42f44b' },
-        headerTintColor: '#fff',
-        headerTitleStyle: { fontWeight: 'bold' },
-      }}>
-      <Stack.Screen
-        name="Home"
-        component={HomeScreen}
-        options={{ title: 'Home Page' }}
-      />
-      <Stack.Screen
-        name="Details"
-        component={SettingsScreen}
-        options={{ title: 'Screen1' }}
-      />
-    </Stack.Navigator>
-  );
+    <>
+      <Tab.Navigator
+        initialRouteName="Home"
+        
+        
+        screenOptions={{
+          tabBarActiveTintColor: '#028373',
+          tabBarStyle: {
+            position: 'relative',
+            backgroundColor: '#fff',
+            height: 100
+
+          },
+          headerLeft: () => <Ionicons name="chevron-back" size={20} color="black" onPress={()=>navigation.goBack()} />,
+
+        }}
+        >
+        <Tab.Screen
+          name="Wallet"
+          component={HomeScreen}
+          options={{
+            tabBarLabel: '',
+            tabBarIcon: ({ color, size }) => (
+              <FontAwesome5 name="wallet" size={30} color="#028373" />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Contacts"
+          component={HomeScreen}
+          options={{
+            tabBarLabel: '',
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons name="contacts" size={30} color="#028373" />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Connect"
+          component={HomeScreen}
+          options={{
+            tabBarLabel: '',
+            tabBarIcon: ({ color, size }) => (
+              <MaterialIcons name="swap-vert-circle" size={60} color="#028373" />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Notification"
+          component={HomeScreen}
+          options={{
+            tabBarLabel: '',
+            tabBarIcon: ({ color, size }) => (
+              <Octicons name="bell" size={30} color="#028373" />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Settings"
+          component={HomeScreen}
+          options={{
+            tabBarLabel: '',
+            tabBarIcon: ({ color, size }) => (
+              <Fontisto name="player-settings" size={30} color="#028373" />
+            ),
+          }}
+        />
+      </Tab.Navigator>
+    </>
+  )
 }
 
-export function SettingsStack() {
-  return (
-    <Stack.Navigator
-      initialRouteName="Settings"
-      screenOptions={{
-        headerStyle: { backgroundColor: '#42f44b' },
-        headerTintColor: '#fff',
-        headerTitleStyle: { fontWeight: 'bold' },
-      }}>
-      <Stack.Screen
-        name="Settings"
-        component={SettingsScreen}
-        options={{ title: 'Setting Page' }}
-      />
-      <Stack.Screen
-        name="Details"
-        component={SettingsScreen}
-        options={{ title: 'Details Page' }}
-      />
-      <Stack.Screen
-        name="Profile"
-        component={SettingsScreen}
-        options={{ title: 'Profile Page' }}
-      />
-    </Stack.Navigator>
-  );
-}
-
-
+export default Navigator
