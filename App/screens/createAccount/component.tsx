9a320ec118ac220bbb5ec0b94fa153e998a4a172
sammykirigha <dkirigha18@gmail.com>
2021-12-24 11:07:45 +0300

    created navigation between screens

---
+++ App/screens/createAccount/component.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const CreateAccount = () => {
+    return (
+        <View>
+            <Text>
+                create a new account in algo expert
+            </Text>
+        </View>
+    )
+}
+
+export default CreateAccount
