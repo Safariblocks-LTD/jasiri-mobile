98c934cd29f68af2792f8db676a287082375965b
sammykirigha <dkirigha18@gmail.com>
2021-12-29 11:14:49 +0300

    created a splash page

--- App/screens/home/component.tsx
+++ App/screens/home/component.tsx
@@ -10,14 +10,29 @@ import {
   Text,
   SafeAreaView,
 } from 'react-native';
+import { black } from 'react-native-paper/lib/typescript/styles/colors';
 
 const HomeScreen = ({ navigation }) => {
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      <View style={{ flex: 1, padding: 16 }}>
-        <View
+      <View style={{ flex: 1, padding: 16, width: 375, height: 812, backgroundColor: '#E3E8E7'
+ }}>
+     <View style={{width: 170, height: 163, overflow: 'hidden', backgroundColor: '#403131', top: -56, left: -24, position: 'absolute', opacity: .1, borderRadius: 130}}></View>
+     <View style={{width: 170, height: 163, left: -59, backgroundColor: '#8E5858', opacity: .05, position: 'absolute', borderRadius: 100}}></View>
+     <View>
+       <Text>
+       JA$IRI 
+       </Text>
+     </View>
+     <View>
+     <TouchableOpacity
+            style={styles.button}
+            onPress={() => navigation.navigate('CreateAccount')}>
+            <Text>add account</Text>
+          </TouchableOpacity>
+     </View>
+        {/* <View
           style={{
-            flex: 1,
             alignItems: 'center',
             justifyContent: 'center',
           }}>
@@ -27,7 +42,7 @@ const HomeScreen = ({ navigation }) => {
               textAlign: 'center',
               marginBottom: 16,
             }}>
-            You are on Home Screen please navigate......
+            You are on Splash Screen?????......
           </Text>
           <TouchableOpacity
             style={styles.button}
@@ -42,17 +57,16 @@ const HomeScreen = ({ navigation }) => {
             <Text>create account</Text>
           </TouchableOpacity>
           <TouchableOpacity
-            style={styles.button}
+            style={styles.account}
             onPress={() => navigation.navigate('Transactions')}>
-            <Text>Transactions</Text>
+            <Text>ADD ACCOUNT</Text>
           </TouchableOpacity>
-        </View>
-        <Text style={{ fontSize: 18, textAlign: 'center', color: 'grey' }}>
-          React Native Bottom Navigation
-        </Text>
-        <Text style={{ fontSize: 16, textAlign: 'center', color: 'grey' }}>
-          www.aboutreact.com
-        </Text>
+          <TouchableOpacity
+            style={styles.account}>
+            <Text>ADD ACCOUNT</Text>
+          </TouchableOpacity> */}
+        {/* </View>
+       <View style={{width: 40, backgroundColor: '#DDDDDD', borderRadius: 50, height: 23 }}></View> */}
       </View>
     </SafeAreaView>
   );
@@ -61,10 +75,40 @@ const HomeScreen = ({ navigation }) => {
 const styles = StyleSheet.create({
   button: {
     alignItems: 'center',
-    backgroundColor: '#DDDDDD',
+    backgroundColor: '#3BD5C2',
     padding: 10,
-    width: 300,
-    marginTop: 16,
+    width: 325,
+    left: 15,
+    bottom: -530,
+    height: 42,
+    // marginTop: 16,
+    borderRadius: 15,
+    position: 'absolute',
+    fontFamily: 'Open Sans',
+    fontStyle: 'normal',
+    fontWeight: 'bold',
+    fontSize: 18,
+    lineHeight: 16,
+    textTransform: 'uppercase'
+    // left: 32.8%,
+    // right: 29.87%,
+    // top: 89.16%,
+    // bottom: 8.87%,
+    // fontfamily: Open Sans,
+    // font-style: normal,
+    // font-weight: bold,
+    // font-size: 18px,
+    // line-height: 16px,
   },
+  account: {
+    width: 325,
+    height: 42,
+    top: 711,
+    left: 25,
+    borderRadius: 15,
+    backgroundColor: '#3BD5C2',
+    color: 'black'
+
+  }
 });
 export default HomeScreen;
