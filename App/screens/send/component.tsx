5f53468cd2f34a72a527c59d29331e7799b6ebe9
sammykirigha <dkirigha18@gmail.com>
2022-01-16 09:09:44 +0300

    added a transact screen

---
+++ App/screens/send/component.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react'
+import { View, Text } from 'react-native'
+
+const Send = () => {
+    return (
+        <View>
+            <Text>
+                send jasiri
+            </Text>
+        </View>
+    )
+}
+
+export default Send
---
+++ App/screens/transact/component.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react'
+import { View, Text } from 'react-native'
+
+const Transact = () => {
+    return (
+        <View>
+            <Text>
+                Transacting everything
+            </Text>
+        </View>
+    )
+}
+
+export default Transact
