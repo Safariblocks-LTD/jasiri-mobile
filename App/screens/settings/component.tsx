5da88f0deef249182d017863f577cdb4ec7faeec
sammykirigha <dkirigha18@gmail.com>
2022-01-17 16:07:58 +0300

    added changes on settings screen

--- App/screens/settings/component.tsx
+++ App/screens/settings/component.tsx
@@ -27,98 +27,101 @@ export const SettingsScreen = ({ navigation }: route) => {
             </View>
 
             <View style={{width: '100%',marginTop: 15, ...styles.content}}>
-                <Text style={{fontSize: 18, fontWeight: 'bold', textTransform: 'capitalize'}}> general settings</Text>
-                <View style={{display: 'flex', flexDirection: 'column', width: '100%', marginTop: 10}}>
-                    <View  style={{display: 'flex', flexDirection: 'row', alignItems: 'center', }}>
+                <Text style={styles.categorty_header}> general settings</Text>
+                <View style={styles.category_container}>
+                    <View  style={styles.category_body}>
                         <Image source={require('../../assets/Vector9.png')} style={{marginLeft: 10}} /> 
-                        <Text style={{marginLeft: 20, fontSize: 14, fontWeight: 'bold', textTransform: 'capitalize'}} >fiat currency</Text>
+                        <Text style={styles.category_text} >fiat currency</Text>
                         <Image style={{marginLeft: 200}} source={require('../../assets/Vector17.png')} />
                     </View>
-                    <View style={{ width: '75%', backgroundColor: 'black', height: 2, marginLeft: 55, marginTop: 25}}></View>
+                    <View style={styles.category_divider}></View>
                 </View>
 
-                <View style={{display: 'flex', flexDirection: 'column', width: '100%', marginTop: 20}}>
-                    <View style={{display: 'flex', flexDirection: 'row', alignItems: 'center', }}>
+                <View style={styles.category_container}>
+                    <View style={styles.category_body}>
                         <Image style={{marginLeft: 10}} source={require('../../assets/Vector10.png')} /> 
-                        <Text  style={{marginLeft: 20, fontSize: 14, fontWeight: 'bold', textTransform: 'capitalize'}}>Language</Text>
+                        <Text  style={styles.category_text}>Language</Text>
                         <Image style={{marginLeft: 220}} source={require('../../assets/Vector17.png')} />
                     </View>
-                    <View style={{ width: '75%', backgroundColor: 'black', height: 2, marginLeft: 55, marginTop: 25}} ></View>
+                    <View style={styles.category_divider} ></View>
                 </View>
 
-                <View style={{display: 'flex', flexDirection: 'column', width: '100%', marginTop: 20}}>
-                    <View style={{display: 'flex', flexDirection: 'row', alignItems: 'center', }}>
+                <View style={styles.category_container}>
+                    <View style={styles.category_body}>
                         <Image style={{marginLeft: 10}} source={require('../../assets/Vector11.png')} /> 
-                        <Text style={{marginLeft: 20, fontSize: 14, fontWeight: 'bold', textTransform: 'capitalize'}}>notificationa</Text>
-                        <Image style={{marginLeft: 210}} source={require('../../assets/Vector17.png')} />
+                        <Text style={styles.category_text}>notificationa</Text>
+                        <Image style={{marginLeft: 205}} source={require('../../assets/Vector17.png')} />
                     </View>
-                    <View style={{ width: '75%', backgroundColor: 'black', height: 2, marginLeft: 55, marginTop: 25}}></View>
+                    <View style={styles.category_divider}></View>
                 </View>
               </View>
 
               <View style={styles.content_body}>
-                <Text> security</Text>
-                <View>
-                    <View>
-                        <Image source={require('../../assets/Vector12.png')} /> 
-                        <Text>set/change password</Text>
-                        <Image source={require('../../assets/Vector17.png')} />
+                <Text style={styles.categorty_header}> security</Text>
+                <View style={styles.category_container}>
+                    <View style={styles.category_body}>
+                        <Image style={{marginLeft: 10,}} source={require('../../assets/Vector12.png')} /> 
+                        <Text style={styles.category_text}>set/change password</Text>
+                        <Image style={{marginLeft: 145,}} source={require('../../assets/Vector17.png')} />
                     </View>
-                    <View></View>
+                    <View style={styles.category_divider}></View>
                 </View>
 
-                <View>
-                    <View>
-                        <Image source={require('../../assets/Vector13.png')} /> 
-                        <Text>biometric authentication</Text>
-                        <Image source={require('../../assets/Vector17.png')} />
+                <View style={styles.category_container}>
+                    <View style={styles.category_body}>
+                        <Image style={{marginLeft: 10,}} source={require('../../assets/Vector13.png')} /> 
+                        <Text style={styles.category_text}>biometric authentication</Text>
+                        <Image style={{marginLeft: 125,}} source={require('../../assets/Vector17.png')} />
                     </View>
-                    <View></View>
+                    <View style={styles.category_divider}></View>
                 </View>
 
-                <View>
-                    <View>
-                        <Image source={require('../../assets/Vector13.png')} /> 
-                        <Text>wallet connect sessions </Text>
-                        <Image source={require('../../assets/Vector17.png')} />
+                <View style={styles.category_container}>
+                    <View style={styles.category_body}>
+                        <Image style={{marginLeft: 10,}} source={require('../../assets/vector14.png')} /> 
+                        <Text style={styles.category_text}>wallet connect sessions </Text>
+                        <Image style={{marginLeft: 120,}} source={require('../../assets/Vector17.png')} />
                     </View>
-                    <View></View>
+                    <View style={styles.category_divider}></View>
                 </View>
 
-                <View>
-                    <View>
-                        <Image source={require('../../assets/Vector15.png')} /> 
-                        <Text>developer settings</Text>
-                        <Image source={require('../../assets/Vector17.png')} />
+                <View style={styles.category_container}>
+                    <View style={styles.category_body}>
+                        <Image style={{marginLeft: 10,}} source={require('../../assets/Vector15.png')} /> 
+                        <Text style={styles.category_text}>developer settings</Text>
+                        <Image style={{marginLeft: 160,}} source={require('../../assets/Vector17.png')} />
                     </View>
-                    <View></View>
+                    <View style={styles.category_divider}></View>
                 </View>
 
-                <View>
-                    <View>
-                        <Image source={require('../../assets/Vector16.png')} /> 
-                        <Text>reset wallet</Text>
-                        <Image source={require('../../assets/Vector17.png')} />
+                <View style={styles.category_container}>
+                    <View style={styles.category_body}>
+                        <Image style={{marginLeft: 10,}} source={require('../../assets/Vector16.png')} /> 
+                        <Text style={styles.category_text}>reset wallet</Text>
+                        <Image style={{marginLeft: 200,}} source={require('../../assets/Vector17.png')} />
                     </View>
-                    <View></View>
+                    <View style={styles.category_divider}></View>
                 </View>
               </View>
 
               <View style={styles.content_main}>
-                <Text> legal</Text>
-                <View>
-                    <View>
-                        <Text>terms of service</Text>
-                        <Image source={require('../../assets/Vector17.png')} />
-                    </View>
-                    <View></View>
-                </View>
-                <View>
-                    <View>
-                        <Text>private policy</Text>
-                        <Image source={require('../../assets/Vector17.png')} />
-                    </View>
-                    <View></View>
+                <Text style={styles.categorty_header}> legal</Text>
+                <View style={{display: 'flex', marginLeft: 50, alignItems: 'flex-start', marginTop: 20, }}>
+                  <View style={styles.category_container}>
+                      <View style={styles.category_body}>
+                          <Text style={styles.category_text}>terms of service</Text>
+                          <Image style={{marginLeft: 150,}} source={require('../../assets/Vector17.png')} />
+                      </View>
+                      <View style={styles.category_divider}></View>
+                  </View>
+
+                  <View style={styles.category_container}>
+                      <View style={styles.category_body}>
+                          <Text style={styles.category_text}>private policy</Text>
+                          <Image style={{marginLeft: 172,}} source={require('../../assets/Vector17.png')} />
+                      </View>
+                      <View style={styles.category_divider}></View>
+                  </View>
                 </View>
               </View>
             </View>
@@ -156,7 +159,48 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'flex-start'
   },
-  content_body: {},
-  content_main: {},
+  categorty_header: {
+    fontSize: 19, 
+    fontWeight: 'bold', 
+    textTransform: 'capitalize',
+    marginLeft: 10
+  },
+  category_container: {
+    display: 'flex', 
+    flexDirection: 'column',
+    width: '100%', 
+    marginTop: 20
+  },
+  category_body: {
+    display: 'flex', 
+    flexDirection: 'row', 
+    alignItems: 'center',
+  },
+  category_text: {
+    marginLeft: 20, 
+    fontSize: 14, 
+    fontWeight: 'bold', 
+    textTransform: 'capitalize'
+  },
+  category_divider: {
+    width: '75%', 
+    backgroundColor: 'black', 
+    height: 2, 
+    marginLeft: 55,
+     marginTop: 25
+  },
+  content_body: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'flex-start',
+    marginTop: 20
+  },
+  content_main: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'flex-start',
+    marginTop: 20,
+    marginBottom: 20
+  },
 });
 
