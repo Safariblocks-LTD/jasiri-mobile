5ef5e0cfa21c848fb90acdb0dce65fb25c7fc102
FAenX <kipronofb@gmail.com>
2022-02-02 17:40:55 +0300

    strip algorand://

--- App/services/algorand.ts
+++ App/services/algorand.ts
@@ -100,10 +100,14 @@ const waitForConfirmation = async function (algodClient, txId, timeout) {
 // createAccount()
 
 export const sendAsset=async (desc: string, amount: number, assetIndex: number, receiver: string)=>{
+    
+    const addrr = receiver.split('//')
+    
+    let stripped = addrr.length > 1?addrr[1]:addrr[0]
 
     try{
         let accountInfo = await algodClient.accountInformation(sender).do();
-        console.log("Account balance: %d microAlgos", accountInfo.amount);
+        
         let startingAmount = accountInfo.amount;
 
 
@@ -113,30 +117,23 @@ export const sendAsset=async (desc: string, amount: number, assetIndex: number, 
         const enc = new TextEncoder();
         const note = enc.encode(desc);
         
-        let closeout = receiver; //closeRemainderTo
-        let txn = algosdk.makeAssetTransferTxnWithSuggestedParams(sender, receiver, undefined, undefined, amount, note, assetIndex, params)
+        let closeout = stripped; //closeRemainderTo
+        let txn = algosdk.makeAssetTransferTxnWithSuggestedParams(sender, stripped, undefined, undefined, amount, note, assetIndex, params)
         // let txn = algosdk.makePaymentTxnWithSuggestedParams(sender, receiver, amount, undefined, note, params);
         const account = algosdk.mnemonicToSecretKey(mnemonic)
 
         // Sign the transaction
         let signedTxn = txn.signTxn(account.sk);
         let txId = txn.txID().toString();
-        console.log("Signed transaction with txID: %s", txId);
 
         // Submit the transaction
         await algodClient.sendRawTransaction(signedTxn).do();
 
         // Wait for confirmation
         let confirmedTxn = await waitForConfirmation(algodClient, txId, 4);       
-        console.log("Transaction " + txId + " confirmed in round " + confirmedTxn["confirmed-round"]);        
         var string = new TextDecoder().decode(confirmedTxn.txn.txn.note);
-        console.log("Note field: ", string);
         accountInfo = await algodClient.accountInformation(sender).do();
-        console.log("Transaction Amount: %d microAlgos", confirmedTxn.txn.txn.amt);        
-        console.log("Transaction Fee: %d microAlgos", confirmedTxn.txn.txn.fee);
         let closeoutamt = startingAmount - confirmedTxn.txn.txn.amt - confirmedTxn.txn.txn.fee;        
-        console.log("Close To Amount: %d microAlgos", closeoutamt);
-        console.log("Account balance: %d microAlgos", accountInfo.amount);
         return 0
 
     }catch(e){
