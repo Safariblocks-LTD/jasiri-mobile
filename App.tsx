e0424c8a1acce460455368b7905217580276167b
FAenX <kipronofb@gmail.com>
2022-03-15 16:46:40 +0300

    update wallet connect screen

--- App.tsx
+++ App.tsx
@@ -4,26 +4,17 @@ import 'fastestsmallesttextencoderdecoder'
 import 'react-native-gesture-handler';
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { NativeBaseProvider, Box } from 'native-base';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
-import { Provider, useDispatch, useSelector } from 'react-redux'
-import {RootState, store}  from './App/redux/store'
+import { NativeBaseProvider } from 'native-base';
+import { Provider, useDispatch } from 'react-redux'
+import {store}  from './App/redux/store'
 // import HomeStack from './App/screens/index'
 import { AuthenticationNavigation
  } from './App/navigation';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 
-import * as Updates from 'expo-updates';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { setAddress, setIsLoggedIn, setMnemonic } from './App/redux';
-
-// import WalletConnectProvider from '@walletconnect/react-native-dapp';
-// import { withWalletConnect } from '@walletconnect/react-native-dapp';
-// import { Linking } from 'react-native';
-
-const uri = 'wc:0ac0146f-13a3-4739-9e85-7eeebf2ac70d@1?bridge=https%3A%2F%2F3.bridge.walletconnect.org&key=5090de44777a909754f2c8ed878a6868cea54e98bfd7d1288351f7628c1575d5'
 
 
 const Navigation=()=>{
@@ -45,17 +36,7 @@ const Navigation=()=>{
           console.log(' error reading value  ')
       }})();
 
-    (async()=>{
-      try {
-      const update = await Updates.checkForUpdateAsync();
-      if (update.isAvailable) {
-        await Updates.fetchUpdateAsync();
-        // ... notify user of update ...
-        await Updates.reloadAsync();
-      }
-    } catch (e) {
-      // handle or log error
-    }})()
+ 
     
   },[])
 
